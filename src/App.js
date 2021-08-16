cc4f47f1d41fc67844534ebda414ee0f4d6d0ba4
Shishir Chulliyil <shishirchandrabose@gmail.com>
2021-08-16 17:42:32 -0400

    initialized react app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { CssBaseline, Grid } from "@material-ui/core";
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // this is a React.Fragment
+    <>
+      <CssBaseline />
+      <Header />
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}>
+          {/* List is going to take 4/12 space in Medium to Large Devices and full 12 space in Small Devices */}
+          <List />
+        </Grid>
+        <Grid item xs={12} md={8}>
+          {/* List is going to take 8/12 space in Medium to Large Devices and full 12 space in Small Devices */}
+          <Map />
+        </Grid>
+      </Grid>
+    </>
   );
-}
+};
 
 export default App;
